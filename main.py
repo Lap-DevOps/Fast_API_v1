72d1b776c234e7a2e15139be406b3c1de8ff87dd
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2023-10-03 13:40:44 +0100

    --jwt login--

--- main.py
+++ main.py
@@ -1,107 +1,43 @@
-from collections import defaultdict
-from datetime import datetime
-from enum import Enum
-from typing import List, Optional
+from fastapi_users import FastAPIUsers
 
-from fastapi import FastAPI, Request, status
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel, Field, ValidationError
+
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
+
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.shemas import UserRead, UserCreate
 
 app = FastAPI(
-    title='Trading App'
+    title="Trading App"
 )
 
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
-@app.exception_handler(ValidationError)
-async def custom_form_validation_error(request, exc):
-    reformatted_message = defaultdict(list)
-    for pydantic_error in exc.errors():
-        loc, msg = pydantic_error["loc"], pydantic_error["msg"]
-        filtered_loc = loc[1:] if loc[0] in ("body", "query", "path") else loc
-        field_string = ".".join(filtered_loc)  # nested fields with dot-notation
-        reformatted_message[field_string].append(msg)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder(
-            {"detail": "Invalid request", "errors": reformatted_message}
-        ),
-    )
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-
-fake_users = [
-    {'id': 1, 'role': 'admin', 'name': ['Bob']},
-    {'id': 2, 'role': 'investor', 'name': 'John'},
-    {'id': 3, 'role': 'trader', 'name': 'Matt'},
-    {'id': 4, 'role': 'investor', 'name': 'HOMER', 'degree': [
-        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
-    ]
-     },
-
-]
-
-fake_trades = [
-    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'amount': 2.12},
-    {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'amount': 2.12},
-]
+current_user = fastapi_users.current_user()
 
 
-@app.get('/')
-def hello():
-    return 'hello world'
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
-class DegreeType(Enum):
-    newbie = 'newbie'
-    expert = 'expert'
-
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type_degree: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: Optional[List[Degree]] =[]
-
-
-@app.get('/users/{user_id}', response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get('id') == user_id]
-
-
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 1):
-    return fake_trades[offset:][:limit]
-
-
-@app.post('/users/{user_id}')
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
-    current_user['name'] = new_name
-    fake_users[current_user['id']]['name'] = new_name
-    return {
-        'status': 200,
-        'data': current_user,
-    }
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: float = Field(ge=0)
-    amount: float
-
-
-@app.post('/trades')
-def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {'status': 200, 'data': fake_trades}
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
